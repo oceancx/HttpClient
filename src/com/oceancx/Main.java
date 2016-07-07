0c8b7f8ab800c0322b592e95957dca2239d309de
oceancx <oceancx@gmail.com>
2016-07-07 22:30:15 +0800

    test

--- src/com/oceancx/Main.java
+++ src/com/oceancx/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         LzHttpClient client = LzHttpClient.get();
-        client.getRequest("http://www.baidu.com");
+        client.getRequest("http://www.xiaomi.com");
     }
 }
